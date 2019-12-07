a93a21a361f367e7c5ba41030a1973d58b0d1381
uyenuyenvu <uyenvt.vnua@gmail.com>
2019-12-07 23:04:03 +0700

    unit 2

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,48 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/','home');
+Route::view('/home','home');
+
+//user
+Route::get('/user/{id?}', function ($id=null) {
+	if($id==null){
+		echo "all";
+	}else{
+		echo "user ".$id;
+	}
+});
+
+//thành công
+Route::get('/thanhcong/{id}', function($id){
+	dd('thành công'.$id);
+});
+
+
+
+Route::prefix('task')->group(function(){
+	//delete
+	Route::delete('/deleted/{id}',function($id){
+		return redirect("/thanhcong/".$id);
+	})->name('todo.task.delete');
+	//edit
+	Route::get('/edit/{id}',function($id){
+		dd('edit');
+	})->name('todo.task.edit');
+	//finish
+	Route::get('/finish/{id?}',function($id=null){
+		if($id==null){
+			echo "finish all";
+		}else{
+			echo "finish ".$id;
+		}
+		})->name('todo.task.finish');
+	//again
+	Route::get('/again/{id?}',function($id=null){
+		if($id==null){
+			echo 'do again all!';
+		}else{
+			echo "again ".$id;
+		}
+	})->name('todo.task.again');
 });
